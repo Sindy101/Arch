65097dd2c23c6aeb111a56d55b3dac5e9ca2d005
Sindy101 <glebon.belous.2003@mail.ru>
2025-04-14 17:32:35 +0300

    1

---
+++ dwm/themes/mhtheme.h
@@ -0,0 +1,8 @@
+// mh's them
+// clang-format off
+
+static const char col_gray1[] = "#040D12";
+static const char col_gray2[] = "#040D12";
+static const char col_gray3[] = "#FCF596";
+static const char col_gray4[] = "#040D12";
+static const char col_accnt[] = "#4B4376";
